68736c145af9f884fd4464a98b3c13da7e69ce55
Begum.Yazici <yazicibegum@gmail.com>
2017-10-10 16:44:08 +0300

    Layout design

---
+++ app/GuideApplication/app/src/main/java/guideapplicaiton/begi/com/guideapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package guideapplicaiton.begi.com.guideapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        //// TODO: 10.10.2017  
+    }
+}
